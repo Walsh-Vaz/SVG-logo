1c4b98d3669cb95f7653cf50b80151cf51c27201
Walsh Vaz <walshivaz@yahoo.com>
2023-04-18 17:27:01 -0400

    added testing for the shapes

--- test/makeShapesTest.js
+++ test/makeShapesTest.js
@@ -1,4 +1,4 @@
-const {Circle, Rectangle, Triangle} = require("./makeShapes");
+const {Circle, Rectangle, Triangle, SVG} = require("./makeShapes");
 
 // trinagle 
 
@@ -6,7 +6,7 @@ describe ("triangle set", () => {
     test("makes sure it renders correctly", () => {
         const t = new Triangle;
         t.colourSetting("blue");
-        expect(shape.rendering()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+        expect(t.rendering()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
     } )
 })
 
@@ -16,7 +16,7 @@ describe ("Circle set", () => {
     test("makes sure it renders correctly", () => {
         const c = new Circle;
         c.colourSetting("blue");
-        expect(shape.rendering()).toEqual('<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="blue"/>');
+        expect(c.rendering()).toEqual('<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="blue"/>');
     } )
 })
 
@@ -26,6 +26,19 @@ describe ("rectangle  set", () => {
     test("makes sure it renders correctly", () => {
         const r = new Rectangle;
         r.colourSetting("blue");
-        expect(shape.rendering()).toEqual('<rect x="50" y="50" width="300" height="300" stroke="black" fill="blue" stroke-width="5"/>');
+        expect(r.rendering()).toEqual('<rect x="50" y="50" width="300" height="300" stroke="black" fill="blue" stroke-width="5"/>');
+    })
+})
+
+//SVG
+
+describe ("SVG  set", () => {
+    test("makes sure it renders correctly", () => {
+        const s = new SVG;
+        const sh = new Circle;
+        const text = s.setTextElement("sds", "black");
+        const shape = s.makeShape(sh)
+
+        expect(s.rendering()).toEqual('<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">' + shape + text + '</svg>');
     })
 })
